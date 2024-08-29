864504325fbfad5a9eeb8a0e15b200b707853689
сергей оразаев <cyperman@mail.ru>
2024-08-30 02:28:59 +0300

    Реализовал два интерфейса

---
+++ Classes/FoodData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodFit.Classes
+{
+    internal class FoodData : IFoodData
+    {
+        private double calories;
+        private double proteins;
+        private double fats;
+        private double carbohydrates;
+
+        FoodData (double calories, double proteins, double fats, double carbohydrates)
+        {
+            this.calories = calories;
+            this.proteins = proteins;
+            this.fats = fats;
+            this.carbohydrates = carbohydrates;
+        }
+
+        public double GetCalories() => calories;
+        public double GetCarbohydrates() => carbohydrates;
+        public double GetFats() => fats;
+        public double GetProteins() => proteins;
+    }
+}
---
+++ Classes/UserInput.cs
@@ -0,0 +1,36 @@
+﻿using FoodFit.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodFit.Classes
+{
+    internal class UserInput : IUserInput
+    {
+        public UserInput()
+        {
+        }
+
+        public int GetAge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetGender()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetHeight()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetWeight()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
